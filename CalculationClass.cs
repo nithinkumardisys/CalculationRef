62c020ae83d86c78e47a740a4296e4282afc3567
Nithinkumar.Ramasamy <Nithinkumar.Ramasamy@DISYS.om>
2022-09-14 16:11:37 +0530

    test

--- CalculationClass.cs
+++ CalculationClass.cs
@@ -10,7 +10,9 @@ namespace CalculationRef
     {
         public int AddTwoNumbers(int number1, int number2)
         {
-            return number1 + number2;
+            int result= number1 + number2;
+            return result;
+
         }
     }
 }
